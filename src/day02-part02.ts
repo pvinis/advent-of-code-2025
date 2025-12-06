9a2d6c03b81fe8f5e3e17d2a567e8a1a337c8557
Pavlos Vinieratos <pvinis@gmail.com>
2025-12-06 22:55:31 +0200

    done

--- src/day02-part02.ts
+++ src/day02-part02.ts
@@ -4,6 +4,42 @@ main()
 
 async function main() {
 	const lines = await readLines(process.env.INPUT_FILE!)
+	const input = lines[0]
+	const pairs = input.split(",")
 
-	console.log(lines)
+	const invalids: number[] = []
+
+	for (const pair of pairs) {
+		const [lowStr, highStr] = pair.split("-")
+		const low = parseInt(lowStr)
+		const high = parseInt(highStr)
+
+		for (let n = low; n <= high; n++) {
+			const str = n.toString()
+
+			if (isRepeating(str)) {
+				invalids.push(n)
+			}
+		}
+	}
+
+	const sum = invalids.reduce((a, b) => a + b, 0)
+
+	console.log("answer:")
+	console.log(sum)
+}
+
+function isRepeating(str: string): boolean {
+	const len = str.length
+	for (let patternLen = 1; patternLen <= len / 2; patternLen++) {
+		if (len % patternLen !== 0) continue
+
+		const pattern = str.slice(0, patternLen)
+		const repeated = pattern.repeat(len / patternLen)
+
+		if (repeated === str) {
+			return true
+		}
+	}
+	return false
 }
